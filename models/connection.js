6ded8d0113932e1af6f1022d99ec43807c072fe2
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-13 13:04:19 -0400

    create mongoose connections

--- models/connection.js
+++ models/connection.js
@@ -1,33 +1,15 @@
-/* Import the mongoose module
- *
- */
+// connect mongoose to project
 const mongoose = require('mongoose');
 
-/* Step 1.
- *
- * TODO: replace <db-name> with the name of your mongo database. 
- * This will need to change for every new project you create.
- *
- */
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-name>";
+// create MongoDB environament
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost/sei-chatbot";
 
 
-/* Step 2
- *
- * Open up a connection to the mongo database.
- *
- * NOTE: newUrlParser diables a deprecation warning
- */
+// create connection to mongodb and mongooose
 mongoose.connect(connectionString, { useNewUrlParser: true})
   .then(() => {
     console.log("connected to mongo at: " + connectionString);
   });
 
-
-/* Export the mongoose object.
- *
- * This will allow us to use the same connection to our DB
- * across our different controllers.
- *
- */
+// will allow us to use mongoDb in our controllers
 module.exports = mongoose
