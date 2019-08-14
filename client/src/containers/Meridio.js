fc3abbe92348d4a1b8f8e9aad631348065ceccbb
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-14 11:16:10 -0400

    add meridio container

---
+++ client/src/containers/Meridio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Jumbotron } from 'reactstrap';
+import './Meridio.css'
+
+export default class Meridio extends Component {
+    render() {
+        return (
+            <div className='col-lg-5 col-sm-12 Meridio'>
+                <Jumbotron>
+
+                </Jumbotron>
+            </div>
+        )
+    }
+}
