5889a4b6f966b06c0f0d2ecb0fde7e6e7021508d
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-14 11:00:44 -0400

    create Mainbody container

---
+++ client/src/containers/MainBody.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Meridio from './Meridio';
+import Media from './Media'
+import { Container } from 'reactstrap'
+export default class MainBody extends Component {
+
+    // Create
+    state = {
+        content: [],
+        isMeridioActive: false,
+        subject: null
+    }
+
+    getContent = async (str) => {
+        let response = await axios.get(`/api/v1/questions`)
+        await this.setState({ content: response.data })
+    }
+
+    componentDidMount() {
+        this.getContent()
+    }
+
+    render() {
+        //destructure state
+        let { content } = this.state
+
+        return (
+            <Container>
+                <div className='row' >
+                    <Media content={content} />
+                    <Meridio />
+                </div>
+            </Container>
+        )
+    }
+}
