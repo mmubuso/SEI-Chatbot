8a624934b572e05c3d4dfaca5a000e6cb720e1d3
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-14 11:01:39 -0400

    create Media component

---
+++ client/src/containers/Media.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Media.css'
+import { Jumbotron } from 'reactstrap';
+
+export default class Media extends Component {
+    render() {
+        return (
+            <div className='col-lg-8 col-sm-12 Media'>
+                <Jumbotron>
+                    
+                </Jumbotron>
+            </div>
+        )
+    }
+}
