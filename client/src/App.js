cb519b4778dd995dc1e2e56ac9e12e282c369cae
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-14 11:02:15 -0400

    change Media name to MainBody

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar.js';
+import MainBody from './containers/MainBody';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        
+        <MainBody />
       </Router>
     </div>
   );
