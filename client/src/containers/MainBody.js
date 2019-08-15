d02351172bc62429853f7153558db69c4446b054
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-15 11:00:02 -0400

    add method to filter through selections

--- client/src/containers/MainBody.js
+++ client/src/containers/MainBody.js
@@ -10,7 +10,6 @@ export default class MainBody extends Component {
     // Create
     state = {
         subjects: [],
-        isMeridioActive: false,
         information: [],
         subject: null
     }
@@ -20,19 +19,29 @@ export default class MainBody extends Component {
         let response = await axios.get('api/v1/subjects')
         this.setState({ subjects: response.data })
     }
-    //This method gets all information about one specific id
+    //Gets all information about one specific id
     //Input 
     //  subjectId - string of mongoId for subject
     //  information -  string that should only either be 'resources' or 'questions'
     getAllInformationForChosenSubject = async (subjectId, information) => {
         let response = await axios.get(`api/v1/subjects/${subjectId}/${information}`)
-        this.setState({ information: response.data })
+        await this.setState({ information: response.data })
     }
 
-
+    //Filter through information for everything that has a specific input
+    //Input
+    // topic - string of topic we are looking for
+    filterForTopic = (topic) => {
+        let filteredArray = this.state.information.filter(info => {
+            return info.questions === topic
+        })
+        this.setState({ information: filteredArray })
+    }
 
     componentDidMount() {
         this.getAllSubject()
+        // For testing purposes
+        // this.getAllInformationForChosenSubject("5d54a165ec4bf85d8628c4e6","questions")
     }
 
     render() {
