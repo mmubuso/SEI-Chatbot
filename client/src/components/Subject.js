69c826a8e67c3db1675ab8e3f5c5fe23da79e4d2
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-15 21:34:16 -0400

    update styling and added functions via props

--- client/src/components/Subject.js
+++ client/src/components/Subject.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import './Subject.css'
 
 export default class Subject extends Component {
+
+    runTwoMethods = async (id) => {
+        await this.props.subjectMethod(id)
+        this.props.toggle()
+    }
+
     render() {
-        
+        //destructure props
+        let { subjects } = this.props
+
+        //create jsx elements for subjects
+        let subjectList = subjects.map(subject => {
+            return (
+                <button
+                    key={subject._id}
+                    className='subject col-sm-12'
+                    onClick={() => this.runTwoMethods(subject._id)}>
+                    {subject.name}
+                </button>
+            )
+        })
         return (
-            <div>
-                
+            <div className='Subjects mb-4'>
+                <h1 className='display-3'>Choose a topic to learn about</h1>
+                {subjectList}
             </div>
         )
     }
