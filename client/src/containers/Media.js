dcd8e842f1ada6f51e3964c28856d300b1becdca
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-16 10:47:16 -0400

    pass down questions and resources to info component

--- client/src/containers/Media.js
+++ client/src/containers/Media.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Media.css'
 import { Jumbotron } from 'reactstrap';
+import Information from '../components/Information';
 
 
 // Media is the compoenent that will hold all the questions and information
@@ -13,17 +14,34 @@ export default class Media extends Component {
 
 
     render() {
-        
-        //Checks if mainbody context is empty
-        // this.props.context.map()
 
-        //Destructure media
+        //Destructure props and state
+        let { information } = this.props;
+        let { } = this.state;
+
+        //Create list of items
+        let informationList = information.map(info => {
+            return (
+                <Information
+    
+                    key={info._id}
+                    infoTitle={info.questions ? info.questions : info.topic}
+                    info1={info.optionA ? info.optionA : info.resourceA}
+                    info2={info.optionB ? info.optionB : info.resourceB}
+                    info3={info.optionC ? info.optionC : info.resourceC}
+                    info4={info.optionD ? info.optionD : info.resourceD}
+
+                />
+            )
+        })
+
+
+
         return (
             <div className='col-md-7 col-sm-12 Media'>
                 <Jumbotron>
                     <div className='MediaContainer'>
-                        <h1>{}</h1>
-
+                        {informationList}
                     </div>
                 </Jumbotron>
             </div>
