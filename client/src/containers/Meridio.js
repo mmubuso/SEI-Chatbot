2aa64ca6a55f0fc1949bd2c1fd4247acc98cc7f7
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-15 21:35:21 -0400

    add flag to toggle between messages and subject

--- client/src/containers/Meridio.js
+++ client/src/containers/Meridio.js
@@ -11,19 +11,27 @@ export default class Meridio extends Component {
         entities: {},
         userInput: '',
         meridioFlag: false,
-        messages: []
+        messages: [],
+        messageFlag: false
+    }
+
+    main = () => {
+        console.log(this.valdateWitIntent() + ' ' + this.valdateWitTopic())
+        if(this.valdateWitIntent() && this.valdateWitTopic()){
+            this.pushUserInputToMessages('Meridio: Generating requested information')
+        }
     }
 
     //Send userinput to wit api
     //Input
     // userInput - String of users question
-    callWitApi = async (userInput) => {
-        userInput = userInput.replace(/[ ]/g, '%20')
+    callWitApi = async () => {
+        let userInput = this.state.userInput.replace(/[ ]/g, '%20')
         let response = await axios({
             method: 'GET',
             url: `https://api.wit.ai/message?v=20190815&q=${userInput}`,
             headers: {
-                Authorization: `Bearer `,
+                Authorization: `Bearer 4E6O2KLWJIQ4G7M3YG564NJR4BO7CSBY`,
             }
         })
         this.setState({ entities: response.data.entities })
@@ -36,7 +44,8 @@ export default class Meridio extends Component {
 
     // Validate to make sure entities has topic
     valdateWitTopic = () => {
-        return this.state.entities.topic ? true : false
+        // return this.state.entities.topic ? true : false
+        return true
     }
 
 
@@ -57,32 +66,40 @@ export default class Meridio extends Component {
     }
 
     //When user submits a phrase
-    handleOnSubmit = (event) => {
+    handleOnSubmit = async (event) => {
         event.preventDefault()
-        this.pushUserInputToMessages()
+        await this.callWitApi()
+        this.pushUserInputToMessages(`Human: ${this.state.userInput}`)
+        this.main()
     }
 
     //Push user message to list of messages
-    pushUserInputToMessages = () => {
-        let input = this.state.userInput
+    pushUserInputToMessages = (input) => {
         let newMessage = [...this.state.messages]
         newMessage.push(input)
         this.setState(
-            { 
+            {
                 messages: newMessage,
                 userInput: ''
-        })
+            })
     }
 
+    //Toggle show message of or on
+    togggleMessageScreen = () => {
+        this.setState((state) => {
+            return { messageFlag: !state.messageFlag }
+        })
+    }
     componentDidMount() {
-        //Use to test api call
-        // this.callWitApi('What will you open the store')
-        //Ask user to select subject
+        this.pushUserInputToMessages(`Meridio: Hello welcome to the SEI learning platform, 
+        My name is Meridio. I'm really good at helping you find information on Javascript 
+        data typs and control flow. 
+        Try asking me, "I want to learn about primitive data types"`)
     }
     render() {
 
         //destructure state
-        let { userInput, entities, messages } = this.state
+        let { userInput, messageFlag, messages } = this.state
 
         //destructure props
         let { filterMethod, allInformation, subjects, singleObjectInfo } = this.props
@@ -90,16 +107,23 @@ export default class Meridio extends Component {
         return (
             <div className='col-md-5 col-sm-12 Meridio'>
                 <Jumbotron>
-                    <Messages
-                        messages={messages}
-                    />
-                    <Subject 
-                        subjects={subjects}
-                        subjectMethod={singleObjectInfo}
-                    />
+                    {
+                        messageFlag ?
+                            <Messages
+                                messages={messages}
+                                toggle={this.togggleMessageScreen}
+                            />
+                            :
+                            <Subject
+                                toggle={this.togggleMessageScreen}
+                                subjects={subjects}
+                                subjectMethod={singleObjectInfo}
+                            />
+                    }
+
                     <form onSubmit={(evt) => this.handleOnSubmit(evt)}>
                         <input type='text' value={userInput} onChange={this.handleUserInput} />
-                        <input type='submit' onSubmit={(evt) => this.handleOnSubmit(evt)}/>
+                        <input type='submit' onSubmit={(evt) => this.handleOnSubmit(evt)} />
                     </form>
                 </Jumbotron>
             </div>
