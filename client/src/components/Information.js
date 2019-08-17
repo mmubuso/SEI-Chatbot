ddfdad60ebb48c803c8e50ba7dcfd553c994406b
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-17 17:47:24 -0400

    add delete functionality

--- client/src/components/Information.js
+++ client/src/components/Information.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import './Information.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default class Information extends Component {
+
+
+    deleteItem = () => {
+        axios.delete()
+    }
+
     render() {
 
         //destructure props
-        let { info1, info2, info3, info4, infoTitle} = this.props
+        let { info1, info2, info3, info4, infoTitle, subjectId, itemId, category } = this.props
+
+
+        //create structure to be sent to backend
+
+
         return (
             <div className='Information'>
+
                 <h1 className='lead'>{infoTitle}</h1>
                 <li>{info1}</li>
                 <li>{info2}</li>
                 <li>{info3 ? info3 : 'N/A'}</li>
                 <li>{info4 ? info4 : 'N/A'} </li>
-                <hr class="my-4"></hr>
+                <Link to={`/resource/${itemId}/${category}/${subjectId}`}>Edit Info</Link>
+                <button
+                    onClick={() => this.props.deleteMethod(subjectId,category,itemId)}>Delete</button>
+                <hr className="my-4"></hr>
+
             </div>
         )
     }
