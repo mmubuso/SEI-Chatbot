6dd355d36e5de5f8d02ba38528d361788cbb797a
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-17 17:31:58 -0400

    add switch and route paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar.js';
 import MainBody from './containers/MainBody';
+import EditForm from './components/EditForm';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <MainBody />
+        <Switch>
+          <Route exact path='/' component={MainBody}/>
+          <Route path='/resource/:resourceId/:category/:subjectId' component={EditForm} />
+        </Switch>
       </Router>
     </div>
   );
