b0a5308378adefc92c43e26e271c06cd3766c005
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-18 13:03:47 -0400

    add file with meridio replies

---
+++ client/src/lib/meridiosReplies.js
@@ -0,0 +1,13 @@
+// Greeting replies
+// 
+greetingReply = [
+    "Hey my name is Meridio",
+    "Hello human, what would you like to learn today",
+    "Howdy",
+    "Hows your day going, just kidding I don't care. I'm here to teach you not be your buddy",
+    "I am the smartest bot in the world, ask any questions... about data types or control flow",
+    "Hey bot here, what do you want?"
+]
+
+//Greetings 
+//
